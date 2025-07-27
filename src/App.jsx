7d9a5ab6d13a3fe2cc6151554c97a34803e50021
jpetti777 <jeanette.petti@gmail.com>
2025-07-27 17:50:04 +0000

    padding attempt 2

--- src/App.jsx
+++ src/App.jsx
@@ -1078,7 +1078,7 @@ const LPCProjectEvaluationSurvey = () => {
         </div>
 
         {/* Scrollable Content with extra bottom padding */}
-        <div className="max-w-6xl mx-auto p-4 md:p-6 pb-80">
+        <div className="max-w-6xl mx-auto p-4 md:p-6 pb-96 md:pb-80">
           <div className="space-y-4 md:space-y-6 mb-8">
             {sortedProjects.map(project => {
               const eval_ = evaluations[project.id];
